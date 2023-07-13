0026f198b49cab3fc1a34bb2edcf0f9ff32c7802
neto Safronov <rom.exe@mail.ru>
2023-07-13 20:53:26 +0800

    ZD1_smart_superhero

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def her_vx(her1, her2, her3):
+    compared ={her1:'', her2:'', her3:''}
+    url = "https://akabab.github.io/superhero-api/api/all.json"
+    heroes = requests.get(url).json()
+    for heroi in heroes:
+        for i in compared.keys():
+            if heroi['name'] == i:
+                compared[i] = heroi['powerstats']['intelligence']
+    top = sorted(compared, key=compared.get, reverse=True)
+    return print(f'Самый умный супергерой "{top[0]}"\n')
+print('ЗД1:')
+her_vx('Hulk', 'Captain America', 'Thanos')
