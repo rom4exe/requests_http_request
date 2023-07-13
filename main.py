504f215a2207fd5c8b1a5052793009dff911f595
neto Safronov <rom.exe@mail.ru>
2023-07-13 20:57:30 +0800

    ZD2_YaUploader

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-
+import os
 
 def her_vx(her1, her2, her3):
     compared ={her1:'', her2:'', her3:''}
@@ -13,3 +13,34 @@ def her_vx(her1, her2, her3):
     return print(f'Самый умный супергерой "{top[0]}"\n')
 print('ЗД1:')
 her_vx('Hulk', 'Captain America', 'Thanos')
+
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def upload(self, file_path: str):
+        """Метод загружает файлы по списку file_list на яндекс диск"""
+        file_list = os.listdir(path_to_file)
+        for name in file_list:
+            url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+            headers = {'Authorization': 'OAuth ' + token}
+            param = {'path': '/uploaded_files/'+name}
+            respons = requests.get(url, headers=headers, params=param)
+            data = respons.json()
+            if respons.status_code != 200:
+               print(data['message'])
+            else:
+                url = data['href']
+                with open(file_path+name, 'rb') as f:
+                    requests.post(url, files={'file': f})
+                    print('Файлы загружены')
+        print('')
+
+if __name__ == '__main__':
+    print('ЗД2:')
+    # Получить путь к загружаемому файлу и токен от пользователя
+    path_to_file = 'C:\\Users\\rom.exe\\Documents\\in_ya.d\\'
+    token = ""
+    uploader = YaUploader(token)
+    result = uploader.upload(path_to_file)
